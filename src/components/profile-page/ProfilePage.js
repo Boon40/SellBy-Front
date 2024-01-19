0e98e2c41ed5bebf0b6e45c34c319207df4e5351
yosif <y.yosifov@student.fontys.nl>
2024-01-19 19:48:37 +0100

    final version

--- src/components/profile-page/ProfilePage.js
+++ src/components/profile-page/ProfilePage.js
@@ -35,7 +35,7 @@ const ProfilePage = () => {
               return prevUser;
             });
             try {
-              const currentResponse = await axios.get(`/api/v1/users/email/${jwtDecode(user.token).sub}`);
+              const currentResponse = await axios.get(`http://localhost:8080/api/v1/users/email/${jwtDecode(user.token).sub}`);
               setUserDetails(currentResponse.data);
       
               // Set sellerId in formData directly
@@ -43,7 +43,7 @@ const ProfilePage = () => {
                 ...prevFormData,
                 sellerId: currentResponse.data.id,
               }));
-              console.log("current used")
+              console.log("current user")
               console.log(currentResponse.data.id);
             } catch (error) {
               console.error('Error getting user: ', error);
